13c98e08029dffa5e87c0be544d079aee4a17473
Peter Tran <pettran866@gmail.com>
2022-06-06 20:29:00 -0700

    user options

--- index.js
+++ index.js
@@ -1,3 +1,55 @@
-const mysql = require('mysql2');
 const inquirer = require('inquirer');
 const cTable = require('console.table');
+
+// getting connection
+const db = require('./db/connection');
+
+// Prompt User for choices
+const promptUser = () => {
+  inquirer.prompt([
+    {
+      type: 'list',
+      name: 'choice',
+      message: 'What would you like to do',
+      choices: [
+        'View all departments',
+        'View all roles',
+        'View all employees',
+        'Add a department',
+        'Add a role',
+        'Add an employee',
+        'Update an employee role',
+        'Exit'
+      ]
+    }
+  ])
+  .then(answer => {
+    const {choice} = answer;
+    
+    switch(choice) {
+      case 'View all departments':
+        viewAllDeparments();
+        break;
+      case 'View all roles':
+        viewAllRoles();
+        break;
+      case 'View all employees':
+        viewAllEmployees();
+        break;
+      case 'Add a department':
+        addDeparment();
+        break;
+      case 'Add an employee':
+        addEmployee();
+        break;
+      case 'Update an employee role':
+        updateEmployeeRole();
+        break;
+      default:
+        db.end();
+        return;
+    };
+
+    promptUser();
+  })
+}
